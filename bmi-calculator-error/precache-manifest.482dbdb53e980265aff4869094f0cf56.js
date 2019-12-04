35b901a8c1fe1600e5e5238f912524221d7bf849
Fernando Barrios <me@jfbarrios.com>
2019-12-04 17:26:24 -0600

    bmi calculator con error

---
+++ bmi-calculator-error/precache-manifest.482dbdb53e980265aff4869094f0cf56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbaab75afacaf02499aea6f17fd60de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eb068d0b4f09e86afb5",
+    "url": "/static/css/2.e315d21c.chunk.css"
+  },
+  {
+    "revision": "bd23e19c6afeefdbf96b",
+    "url": "/static/css/main.ec153652.chunk.css"
+  },
+  {
+    "revision": "1eb068d0b4f09e86afb5",
+    "url": "/static/js/2.8e007beb.chunk.js"
+  },
+  {
+    "revision": "bd23e19c6afeefdbf96b",
+    "url": "/static/js/main.4e14e519.chunk.js"
+  },
+  {
+    "revision": "93778619332e0b2db227",
+    "url": "/static/js/runtime-main.e87b19fc.js"
+  }
+]);
