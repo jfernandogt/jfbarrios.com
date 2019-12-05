daf3ee9387a7246e80e2c1e83eacf662cd8c4199
Fernando Barrios <me@jfbarrios.com>
2019-12-04 18:16:45 -0600

    update bmi with error

---
+++ bmi-calculator-error/precache-manifest.34516ec97451ec0cb37030271b67900c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9967b750374235164dcf5fc46b61e911",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "8293a912b58224040af6",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "83c968149dcf98704b52",
+    "url": "/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "8293a912b58224040af6",
+    "url": "/bmi-calculator/static/js/2.920416a7.chunk.js"
+  },
+  {
+    "revision": "83c968149dcf98704b52",
+    "url": "/bmi-calculator/static/js/main.3df5d3bf.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
--- bmi-calculator-error/service-worker.js
+++ bmi-calculator-error/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.482dbdb53e980265aff4869094f0cf56.js"
+  "/bmi-calculator/precache-manifest.34516ec97451ec0cb37030271b67900c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bmi-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
